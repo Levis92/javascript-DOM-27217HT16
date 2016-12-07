f48c64349b12572e69e1ad498d2cad706c4c2930
Anton Levholm <anton.badminton@gmail.com>
2016-12-07 16:00:19 +0100

    Klar med uppgift 2

--- produkter/scripts/calculate.js
+++ produkter/scripts/calculate.js
@@ -14,10 +14,15 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	columnHeaders = pricetable.getElementsByTagName('tr')[0].innerHTML;
 	pricetable.getElementsByTagName('tr')[0].innerHTML = columnHeaders + '<td>Summa</td>';
 
-	// Lägger till columnfält
+	// Lägger till columnfält och onChange-attribut till input-fälten i tabellen
 	tableBodyRows = pricetable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
 	for (var i = 0; i < tableBodyRows.length; i++) {
+
 		tableBodyRows[i].innerHTML = tableBodyRows[i].innerHTML + '<td id="col' + i + '"></td>';
+		tableBodyRows[i].getElementsByTagName('input')[0].setAttribute('onChange', 'updateValue(event)');
+		tableBodyRows[i].getElementsByTagName('input')[0].setAttribute('id', 'qnty' + i);
+
 	}
 
 	// Lägger till summeringsrad
@@ -30,17 +35,34 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	document.getElementById('content').innerHTML = content;
 });
 
+
+// Räknar ut summan för varje produkt och totalsumman
 var calculateSum = function() {
+
 	totalSum = 0, totalQuantity = 0;
+
 	for (var i = 0; i < tableBodyRows.length - 1; i++) {
+
 		var price = tableBodyRows[i].getElementsByTagName('td')[3].innerHTML;
-		var quantity = tableBodyRows[i].getElementsByTagName('td')[4].getElementsByTagName('input')[0].value;
+		var quantity = document.getElementById('qnty' + i).getAttribute('value');
 		quantity = Number(quantity);
 		price *= quantity;
 		document.getElementById('col' + i).innerHTML = price;
+
 		totalSum += price;
 		totalQuantity += quantity;
+
 	}
 	document.getElementById('totalQuantity').innerHTML = totalQuantity;
 	document.getElementById('totalSum').innerHTML = totalSum;
 }
+
+// Uppdaterar value-attributet vid ändring i input-fält
+var updateValue = function(event) {
+	var value = event.target.value;
+	event.target.setAttribute('value', value);
+	
+}
+
+
+
